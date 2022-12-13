7e5471a90e251da62dc38e3be148b53145cf1f52
Hanna Danylova <hanna.danylova@avensia.com>
2022-12-13 20:09:06 +0100

    Add unit tests and mocks

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,6 +6,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
@@ -15,10 +16,8 @@ class CalcTests(SimpleTestCase):
 
         self.assertEqual(res, 11)
 
-
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
-        
